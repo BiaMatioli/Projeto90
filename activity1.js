53e257b57dac92831556af0566b771bea73698b2
BiaMatioli <108369401+BiaMatioli@users.noreply.github.com>
2022-09-29 14:50:24 -0300

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,14 @@
+placar = 0;
+
+function AtualizarPont() {
+    placar = placar + 1;
+    document.getElementById("Placar").innerHTML = placar;
+}
+
+function SalvarPont() {
+    localStorage.setItem("Placar", placar);
+}
+
+function ProxPag() {
+    window.location = "activity_2.html";
+}
